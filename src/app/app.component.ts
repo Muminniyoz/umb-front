5708d605ddb8efc15cc528bc9d4d5de4790b776d
Muminniyoz Shonazarov <muminniyoz2912@gmail.com>
2022-01-18 09:53:23 +0500

    xlsx fayl bilan ishlash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'umb-front';
+export class AppComponent implements AfterViewInit {
+  data = [
+    ['tjf', "bugdo'y navlari",],
+    ['no', 'nomi', 'alomatlari'],
+    [1, 'ksh1', '123']
+  ]
+
+  ngAfterViewInit(): void {
+    /* generate worksheet */
+    // const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    // /* generate workbook and add the worksheet */
+    // const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    // XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    // /* save to file */
+    // XLSX.writeFile(wb, 'SheetJS.xlsx');
+  }
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const ab: ArrayBuffer = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(ab);
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <any>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+    };
+    reader.readAsArrayBuffer(target.files[0]);
+  }
+
 }
