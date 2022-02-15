3c275cf7e36bd59ab8fcf7de300e4dc6f4ffe1ee
Muminniyoz <muminniyoz2912@gmail.com>
2022-02-15 16:06:10 +0500

    rasmlar sozlandi

--- src/app/shared/model/lavozimlar.ts
+++ src/app/shared/model/lavozimlar.ts
@@ -1,12 +1,5 @@
 
 export enum Lavozim{
-    ADMIN = "ADMIN",
-    DIREKTOR = "DIREKTOR",
-    MANAGER = "MANAGER",
-    OSHPAZ = "OSHPAZ",
-    OFITSANT = "OFITSANT",
-    BUXALTER = "BUXALTER",
-    MIJOZ = "MIJOZ",
-    KASSIR = "KASSIR"
+
 
 }
--- src/app/shared/model/tranzaksiya.ts
+++
@@ -1,4 +0,0 @@
-export interface Tranzaksiya{
-    id: number;
-    
-}
